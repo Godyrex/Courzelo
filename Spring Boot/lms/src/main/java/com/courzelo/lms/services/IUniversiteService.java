cf21651a13abba9ccfc50fa486bf0528527a7810
Wissal-Bahri <wissal.bahri@esprit.tn>
2024-02-15 09:28:52 +0100

    Added Entity

---
+++ Spring Boot/lms/src/main/java/com/courzelo/lms/services/IUniversiteService.java
@@ -0,0 +1,11 @@
+package com.courzelo.lms.services;
+
+
+import com.courzelo.lms.entities.Universite;
+
+import java.util.List;
+
+public interface  IUniversiteService {
+   public List<Universite> getuniversities () ;
+
+}
