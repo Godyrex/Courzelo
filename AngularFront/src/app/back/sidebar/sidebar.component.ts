c4f2a27d75bc4b9400f58fb732084af74d0fadf0
Oussema Ouakad <81573320+Godyrex@users.noreply.github.com>
2024-02-29 13:38:58 +0100

    added sidebar image

--- AngularFront/src/app/back/sidebar/sidebar.component.ts
+++ AngularFront/src/app/back/sidebar/sidebar.component.ts
@@ -1,18 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TokenStorageService} from "../../service/user/auth/token-storage.service";
+import {UpdateService} from "../../service/user/profile/update.service";
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
   fullName: string = `${this.token.getUser().name} ${this.token.getUser().lastname}`;
   userRoles = this.token.getUser().roles!;
   auth = this.token.isAuthenticated();
+  userPhotoUrl: any;
 
   constructor(
-    private token: TokenStorageService
+    private token: TokenStorageService,
+    private updateService: UpdateService
   ) {
+  }
+  getImage() {
+    this.updateService.getPhoto(this.token.getUser().photoID!).subscribe((data: Blob) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        this.userPhotoUrl = reader.result;
+      };
+      reader.readAsDataURL(data);
+    });
+  }
+
+  ngOnInit(): void {
+    this.getImage();
   }
 }
