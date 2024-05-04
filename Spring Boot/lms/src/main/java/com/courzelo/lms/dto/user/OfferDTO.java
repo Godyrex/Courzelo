e6683dfe86732961f2119eb42bb21965a6a915f6
Oussema Ouakad <81573320+Godyrex@users.noreply.github.com>
2024-05-04 14:59:54 +0100

    better skills + offer

---
+++ Spring Boot/lms/src/main/java/com/courzelo/lms/dto/user/OfferDTO.java
@@ -0,0 +1,25 @@
+package com.courzelo.lms.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OfferDTO {
+    private String id;
+    private String title;
+    private String description;
+    private String location;
+    private String company;
+    private String salary;
+    private String type;
+    private String experience;
+    private List<String> skills;
+    private String postedOn;
+    private String deadline;
+    private String user;
+    private Float score;
+}
